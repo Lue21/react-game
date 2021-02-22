c3e3de1abd0f8a17b09cd80f36a992831f151913
Katsiaryna Pravasud <pravosud@gmail.com>
2021-02-22 14:50:24 +0300

    feat: add moves count, match pairs stay opened

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import "./App.css";
 import dino from "./dino";
-import DinoCard from './components/DinoCard';
+import DinoCard from "./components/DinoCard";
 import { useEffect, useState } from "react";
 
 const doubleDino = [...dino, ...dino];
 
 function App() {
-
   const [opened, setOpened] = useState([]);
+  const [matched, setMatched] = useState([]);
+  const [moves, setMoves] = useState(0)
 
   useEffect(() => {
-    if (opened.length === 2) 
-      setTimeout(() => setOpened([]), 800)
-  }, [opened])
+    if (opened.length < 2) return;
+
+    const firstCard = doubleDino[opened[0]];
+    const secondCard = doubleDino[opened[1]];
+
+    if (firstCard.id === secondCard.id) {
+      setMatched((matched) => [...matched, firstCard.id]);
+    }
+  }, [opened]);
+
+  useEffect(() => {
+    if (opened.length === 2) setTimeout(() => setOpened([]), 800);
+  }, [opened]);
+
+  useEffect(() => {
+    if (matched.length === dino.length) 
+      alert('You won!')
+  }, [matched])
 
   function flipCard(index) {
+    setMoves((moves) => moves + 1)
     setOpened((opened) => [...opened, index]);
   }
 
-
   return (
     <div className="App">
+      <p>{moves} moves</p>
       <h1>Catch Them All</h1>
       <div className="cards-container">
         {doubleDino.map((dino, index) => {
-          
           let isFlipped = false;
-          
-          if (opened.includes(index))
-            isFlipped = true;
+
+          if (opened.includes(index)) isFlipped = true;
+          if (matched.includes(dino.id)) isFlipped = true;
 
           return (
             <DinoCard
@@ -36,14 +52,13 @@ function App() {
               index={index}
               dino={dino}
               isFlipped={isFlipped}
-              flipCard={flipCard}/>
-            );
+              flipCard={flipCard}
+            />
+          );
         })}
       </div>
     </div>
   );
 }
-
-
 
 export default App;
