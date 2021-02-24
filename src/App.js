4f8e49d98dc9397c1572681fc2c467cd8fe1de06
Katsiaryna Pravasud <pravosud@gmail.com>
2021-02-24 21:38:56 +0300

    feat: add night mode

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import shuffle from "lodash.shuffle";
 import Footer from "./components/footer";
 import Button from "@material-ui/core/Button";
 import Player from "./components/Player";
-import FullscreenIcon from '@material-ui/icons/Fullscreen';
+import FullscreenIcon from "@material-ui/icons/Fullscreen";
+import dinoBig from "./dino";
+
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/Globalstyle";
+import { lightTheme, darkTheme } from "./components/Themes";
+
+import Switch from "@material-ui/core/Switch";
 
 let doubleDino = shuffle([...dino, ...dino]);
+let doubleDinoBig = shuffle([...dinoBig, ...dinoBig]);
 
 function App() {
   const [opened, setOpened] = useState([]);
@@ -18,6 +26,21 @@ function App() {
   );
   const [moves, setMoves] = useState(0);
   const handle = useFullScreenHandle();
+
+  const [theme, setTheme] = useState(localStorage.getItem("SwitchT") || "light");
+  const [state, setState] = useState({
+    checkedB: localStorage.getItem("SwitchN") || false,
+  });
+  const themeToggler = (event) => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+    setState({ ...state, [event.target.name]: event.target.checked });
+    localStorage.setItem("SwitchN", true)
+    localStorage.setItem("SwitchT", 'dark')
+
+
+
+  };
+
 
   useEffect(() => {
     if (opened.length < 2) return;
@@ -45,51 +68,69 @@ function App() {
     setOpened((opened) => [...opened, index]);
   }
   function reset() {
+    setState((state) => false);
+    setTheme((theme) => 'light');
     setMoves((moves) => (moves = 0));
     setOpened((opened) => []);
     setMatched((matched) => []);
     doubleDino = shuffle([...dino, ...dino]);
     localStorage.clear();
-
-    
   }
   return (
-    <div className="App">
-      <h1>Catch Them All</h1>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
 
-      <FullScreen handle={handle}>
-        <div className="nav">
-          <p>{moves} moves</p>
-          <Player />
-          <Button variant="contained" color="primary" onClick={handle.enter}>
-            <FullscreenIcon />
-          </Button>
-          <Button variant="contained" color="primary" onClick={reset}>
-            Reset
-          </Button>
+        <div className="App">
+          <h1>Catch Them All</h1>
+
+          <FullScreen handle={handle}>
+            <div className="nav">
+              <p>{moves} moves</p>
+              <div className="buttons">
+                <Switch
+                  checked={state.checkedB}
+                  onChange={themeToggler}
+                  name="checkedB"
+                  color="primary"
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handle.enter}
+                >
+                  <FullscreenIcon />
+                </Button>
+                <Button variant="contained" color="primary" onClick={reset}>
+                  Reset
+                </Button>
+              </div>
+              <Player />
+            </div>
+
+            <div className="cards-container">
+              {doubleDino.map((dino, index) => {
+                let isFlipped = false;
+
+                if (opened.includes(index)) isFlipped = true;
+                if (matched.includes(dino.id)) isFlipped = true;
+
+                return (
+                  <DinoCard
+                    key={index}
+                    index={index}
+                    dino={dino}
+                    isFlipped={isFlipped}
+                    flipCard={flipCard}
+                  />
+                );
+              })}
+            </div>
+            <Footer />
+          </FullScreen>
         </div>
-
-        <div className="cards-container">
-          {doubleDino.map((dino, index) => {
-            let isFlipped = false;
-
-            if (opened.includes(index)) isFlipped = true;
-            if (matched.includes(dino.id)) isFlipped = true;
-
-            return (
-              <DinoCard
-                key={index}
-                index={index}
-                dino={dino}
-                isFlipped={isFlipped}
-                flipCard={flipCard}
-              />
-            );
-          })}
-        </div>
-        <Footer />
-      </FullScreen>
-    </div>
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/dino.js
+++ src/dino.js
@@ -20,27 +20,49 @@ const dino = [
     },
     {
       id: 6
-    },
-    {
-      id: 7
-    },
-    {
-      id: 8
-    },
-    {
-      id: 9
-    },
-    {
-      id: 10
-    },
-    {
-      id: 11
-    },
-    {
-      id: 12
     }
   
   ]
   
+const dinoBig = [
+  {
+    id: 1
+  },
+  {
+    id: 2
+  },
+  {
+    id: 3
+  },
+  {
+    id: 4
+  },
+  {
+    id: 5
+  },
+  {
+    id: 6
+  },
+  {
+    id: 7
+  },
+  {
+    id: 8
+  },
+  {
+    id: 9
+  },
+  {
+    id: 10
+  },
+  {
+    id: 11
+  },
+  {
+    id: 12
+  }
+  ]
+
 
 export default dino;
+export {dinoBig};
